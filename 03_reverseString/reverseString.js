88f5591be77cdd9853f1e27f6885b39b41cbc070
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-12 21:40:52 +0100

    Fixed bug in reversestring.js and passed tests
    - using jest

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(data) {
+  let newString = "";
+  for (let i = data.length -1; i >=0; i--) {
+    newString += data[i];
+  }
+  return newString;
+  
+
+
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
