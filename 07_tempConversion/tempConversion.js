da648aafc8a3818074ebf4292767cbf9c10b8350
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-14 14:45:36 +0100

    Fixed bug in tempconversion.js
    -passed all tests using jest

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  //Formula (F - 32) * 5/9 = C
+  let celcius = (farenheit - 32) * 5/9;
+  // Round to one decimal place
+  let result = Math.round(celcius * 10 ) / 10;
+  return result;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  //Formula (C * 9/5 ) + 32 = F
+  let farenheit = (celcius * 9/5) + 32;
+  // Round to one decimal place
+  let result = Math.round(farenheit * 10 ) / 10;
+  return result;
+
 };
 
 // Do not edit below this line
