399bbe157b954165923968757cf90281d25a00fe
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-29 06:30:41 +0100

    Solve 08 excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total,item) => total += item,0);
 	
 };
 
-const multiply = function() {
+const multiply = function(...array) {
+  return array.reduce((total, item) => total *= item);
 
-};
 
-const power = function() {
+}
+
+const power = function(num1, num2) {
+  return num1 ** num2;
+
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  if (number===0) {
+    return 1
+  }else {
+    return number * factorial(number - 1);
+  }
+
 	
 };
 
