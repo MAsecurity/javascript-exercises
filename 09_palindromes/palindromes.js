746c3af5cae0dd74163fb37ac180d7a87622e2a1
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-29 08:13:47 +0100

    Solve 09 excercise palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  //Replace all non letters in array;
+  let removeNonLetters = "";
+  let la = "abcdefghijklmnopqrstuvwxyz";
+  let ua = la.toUpperCase();
+  let numbers = "0123456789";
+  let splitString = string.split('');
+  for (let i=0; i < splitString.length; i++) {
+    if(la.includes(splitString[i]) || ua.includes(splitString[i]) || numbers.includes(splitString[i])) {
+      removeNonLetters += splitString[i];
+    }
+  }
+  // Turn new string into array
+  let newArray = removeNonLetters.split("");
+  // reverse string
+  let reveresedArray = newArray.reverse();
+  // join reversed array as a normal string
+  let joinedReversedArray = reveresedArray.join("");
+  // Now return comparison
+  return removeNonLetters.toLowerCase() === joinedReversedArray.toLowerCase();
 };
 
 // Do not edit below this line
