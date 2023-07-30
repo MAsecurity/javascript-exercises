3fc0a6aa85f79ac8fc7ff4ce4d56292f2188c844
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-30 12:54:38 +0100

    Solve fibonacci excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  //convert input to number
+  let number = +num;
+  if(number === 1) {
+    return 1;
+  }else if (number < 0) {
+    return "OOPS";
+  }
+
+  let secondPrevious= 0;
+  let firstPrevious = 1;
+  let sum;
+  for (i=2; i<=number; i++) {
+    sum = secondPrevious + firstPrevious;
+    secondPrevious = firstPrevious;
+    firstPrevious = sum;
+  }
+  return firstPrevious;
+
 
 };
 
