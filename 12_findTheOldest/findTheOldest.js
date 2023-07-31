4b217b9a113953852219434800e1d775d601eaed
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-31 13:32:41 +0100

    Solve findTheOldest excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  return array.reduce((oldest,currentPerson) => {
+    let ageOfOldest = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    let ageOfCurrentPerson = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return (ageOfOldest < ageOfCurrentPerson) ? currentPerson : oldest;
+  });
 
 };
+
+function getAge(birth,death) {
+  if(!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
+  
 
 // Do not edit below this line
 module.exports = findTheOldest;
